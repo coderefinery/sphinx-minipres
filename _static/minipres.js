52cc683a99fb98fe66d6e2537b84e2a4fa2b84ee
Richard Darst <rkd@zgib.net>
2019-08-07 22:12:53 +0300

    initial commit

---
+++ _static/minipres.js
@@ -0,0 +1,120 @@
+// Add goTo method to elements
+// http://stackoverflow.com/questions/4801655/how-to-go-to-a-specific-element-on-page
+(function($) {
+    $.fn.goTo = function() {
+        $('html, body').animate({
+            scrollTop: $(this).offset().top //+ 'px'
+        }, 'fast');
+        return this; // for chaining...
+    }
+})(jQuery);
+
+// NO good way to do this!.  Copy a hack from here
+// https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getQueryParam(name) {
+    var half = location.search.split(name+"=")[1];
+    return half !== undefined ? decodeURIComponent(half.split('&')[0]) : null;
+
+}
+
+function do_scroll(delta) {
+    // scroll `delta` sections forward or backwards
+
+    //var sections = $(".title, .section");
+    //function section_from_element(section) { return section; }
+    var sections = $("h2");
+    console.log(sections);
+    function section_from_element(section) { return section.parentNode; }
+
+    var curPos = -1;
+    for(i=0; i<sections.size(); i++) {
+	//if ( window.pageYOffset < sections[i].getBoundingClientRect()["top"] ) {
+        //console.log(sections[i], sections[i].getBoundingClientRect()["top"])
+	screen_center = window.innerHeight/2;
+	element_top = sections[i].getBoundingClientRect()["top"]
+	element_bottom = sections[i].getBoundingClientRect()["bottom"]
+        if ( element_top < screen_center ) {
+            continue;
+        }
+        //console.info(sections[i], sections[i].getBoundingClientRect()["top"]);
+        curPos = i-1;
+        //console.info(sections[curPos])
+        break;
+    }
+    console.log("cur=", curPos);
+
+    // We didn't find anything - we would scroll off the bottom.
+    if (curPos == -1) {
+	curPos = sections.size() - 1;
+    }
+
+    // Target element we want to scroll to
+    var targetPos = curPos + delta;
+    console.log("target=", targetPos);
+
+    // if we would scroll past bottom, or above top, do nothing
+    if ( targetPos < 0 || targetPos > (sections.size()-1) ) {
+        return;
+    }
+
+    var targetSection = sections[targetPos];
+    var targetSection = section_from_element(targetSection);
+
+    //var top = targetSection.getBoundingClientRect()["y"];
+    var top = $(targetSection).offset()["top"];
+    var height = targetSection.getBoundingClientRect()["height"];
+    var win_height = window.innerHeight;
+    //console.info(top, height, win_height)
+
+    var scroll_to = 0;
+    if (height >= win_height) {
+        scroll_to = top;
+    } else {
+        scroll_to = top - (win_height-height)/3.;
+    }
+    //console.info(top, height, win_height, scroll_to)
+
+    $('html, body').animate({
+        scrollTop: scroll_to //+ 'px'
+    }, 'fast');
+
+}
+
+
+function minipres() {
+    document.addEventListener('keydown', function (event) {
+
+        switch(event.which) {
+        case 37: // left
+            do_scroll(-1);
+            event.preventDefault();
+            return false;
+    	break;
+
+        //case 38: // up
+
+        case 39: // right
+            do_scroll(+1);
+            event.preventDefault();
+            return false;
+    	break;
+
+        //case 40: // down
+
+        default:
+    	return; // exit this handler for other keys
+            }
+    }, true)
+
+    $(".wy-nav-side").remove();
+    $(".wy-nav-content-wrap").css('margin-left', 0);
+    //$("div .section").css('margin-bottom', '50%');
+    $("h2").css('margin-top', '50%');
+}
+
+slideshow = minipres
+
+if (window.location.search.indexOf('minipres')  != -1 ||
+    window.location.search.indexOf('slideshow') != -1 ) {
+    minipres()
+}
---
+++ minipres.py
@@ -0,0 +1,14 @@
+from os.path import abspath, join, dirname
+
+def setup(app):
+    static_path = abspath(join(dirname(__file__), '_static'))
+    app.connect('builder-inited', lambda app: app.config.html_static_path.append(static_path))
+
+    #app.add_stylesheet("minipres.css")
+    app.add_javascript('minipres.js')
+
+    return {
+        'version': '0.1',
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
