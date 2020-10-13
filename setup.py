6fdc65acb4ac7b3dba08c1a8a8fb5d2283a89991
Richard Darst <rkd@zgib.net>
2020-10-13 15:58:01 +0300

    Rename to sphinx-minipres

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sphinx_ext_minipres",
+    name="sphinx_minipres",
     version="0.0.1.dev0",
     author="Richard Darst",
     #author_email="",
     description="Sphinx extension to turn any web page into a lightweight presentation",
     long_description=long_description,
-    url="https://github.com/rkdarst/sphinx_ext_minipres",
+    url="https://github.com/rkdarst/sphinx-minipres",
     packages=setuptools.find_packages(),
     package_data={
-        "sphinx_ext_minipres": ['_static/*'],
+        "sphinx_minipres": ['_static/*'],
         },
     #py_modules=["minipres"],
     classifiers=[
