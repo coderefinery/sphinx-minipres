a422ae0e69bae1170e84f8cdd1cdbd5f68589bdc
Richard Darst <rkd@zgib.net>
2020-10-28 19:00:40 +0200

    sphinx_minipres/__init__: Better detect sphinx version
    - Use duck typing, don't import sphinx.  This removes dependencies.

--- sphinx_minipres/__init__.py
+++ sphinx_minipres/__init__.py
@@ -1,14 +1,12 @@
 from os.path import abspath, join, dirname
 
-import sphinx
-
 from ._version import __version__
 
 def setup(app):
     static_path = abspath(join(dirname(__file__), '_static'))
     app.connect('builder-inited', lambda app: app.config.html_static_path.append(static_path))
 
-    if sphinx.version_info[0] >= 3:
+    if hasattr(app, 'add_js_file'):
         app.add_js_file('minipres.js')
     else:
         #app.add_stylesheet("minipres.css")
